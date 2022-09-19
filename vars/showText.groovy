b1922741f4a09d6947176f4b8cc43497ff0ba30e
Nurmuhammedov Eziz <oasisindesert@gmail.com>
2022-09-19 18:07:53 +0500

    Update showText.groovy

--- vars/showText.groovy
+++ vars/showText.groovy
@@ -1,9 +1,8 @@
 
 def showStr (String firstName, String lastName) {
-  // sh 'echo "first name: ${firstName}, last name: ${lastName}"'
   echo "first name: ${firstName}, last name: ${lastName}"
 }
 
 def showMap (Map config = [:]) {
-  sh 'echo " first name: ${config.firstName}, last name: ${config.lastName}"'
+  echo "first name: ${config.firstName}, last name: ${config.lastName}"
 }
