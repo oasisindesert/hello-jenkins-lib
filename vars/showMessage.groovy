32dc92f4b83c1611c4c0c9f55dc69d77b2f36811
Nurmuhammedov Eziz <oasisindesert@gmail.com>
2022-09-19 18:13:48 +0500

    Update showMessage.groovy

--- vars/showMessage.groovy
+++ vars/showMessage.groovy
@@ -2,3 +2,11 @@
 def call () {
   sh 'echo "Hello world"'
 }
+
+def showStr (String firstName, String lastName) {
+  echo "first name: ${firstName}, last name: ${lastName}"
+}
+
+def showMap (Map config = [:]) {
+  echo "first name: ${config.firstName}, last name: ${config.lastName}"
+}
